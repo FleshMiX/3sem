2d1935970197fae3135b0dfca770ec97291f995d
Alexandr Nikitin <31885817+FleshMiX@users.noreply.github.com>
2020-12-13 12:47:03 +0300

    Signals done

---
+++ RTchampion.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+int cur_pid;
+
+void signal1(int s) {
+  printf("Ot Korobki do nk\n");
+  sleep(1);
+  kill(cur_pid, SIGUSR2);
+}
+
+void signal2(int s) {
+  printf("Luchshe vseh FRTK!!!\n");
+  sleep(1);
+  kill(cur_pid, SIGUSR1);
+}
+
+void run1() {
+  signal1(0);
+  while(1);
+}
+
+int main() {
+
+  signal(SIGUSR1, signal1);
+  signal(SIGUSR2, signal2);
+
+  cur_pid = getpid();
+
+  run1();
+
+  return 0;
+}
